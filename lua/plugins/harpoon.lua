0405e4fae75ca09da52a901164af2605da520e85
Jordan Palmer <jordan.m.palmer@boeing.com>
2024-12-22 16:09:47 -0500

    harpoon config

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,28 +8,28 @@ return {
       require('harpoon').setup()
 
       -- Keymaps
-      vim.keymap.set('n', '<leader>a', function()
+      vim.keymap.set('n', '<leader>dd', function()
          mark.add_file()
       end, { desc = 'Add file to Harpoon' })
 
-      -- vim.keymap.set('n', '<C-e>', function()
-      --    ui.toggle_quick_menu()
-      -- end, { desc = 'Toggle Harpoon menu' })
+      vim.keymap.set('n', '<leader>de', function()
+         ui.toggle_quick_menu()
+      end, { desc = 'Toggle Harpoon menu' })
 
-      -- vim.keymap.set('n', '<C-h>', function()
-      --    ui.nav_file(1)
-      -- end, { desc = 'Navigate to Harpoon file 1' })
+      vim.keymap.set('n', '<leader>dc', function()
+         ui.nav_file(1)
+      end, { desc = 'Navigate to Harpoon file 1' })
 
-      -- vim.keymap.set('n', '<C-t>', function()
-      --    ui.nav_file(2)
-      -- end, { desc = 'Navigate to Harpoon file 2' })
+      vim.keymap.set('n', '<leader>dx', function()
+         ui.nav_file(2)
+      end, { desc = 'Navigate to Harpoon file 2' })
 
-      -- vim.keymap.set('n', '<C-n>', function()
-      --    ui.nav_file(3)
-      -- end, { desc = 'Navigate to Harpoon file 3' })
+      vim.keymap.set('n', '<leader>ds', function()
+         ui.nav_file(3)
+      end, { desc = 'Navigate to Harpoon file 3' })
 
-      --       vim.keymap.set('n', '<C-s>', function()
-      --          ui.nav_file(4)
-      --       end, { desc = 'Navigate to Harpoon file 4' })
+      vim.keymap.set('n', '<leader>dr', function()
+         ui.nav_file(4)
+      end, { desc = 'Navigate to Harpoon file 4' })
    end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -34,5 +34,10 @@ return {
       keymap.set('n', '<leader>fs', '<cmd>Telescope live_grep<cr>', { desc = 'Find string in cwd' })
       keymap.set('n', '<leader>fc', '<cmd>Telescope grep_string<cr>', { desc = 'Find string under cursor in cwd' })
       keymap.set('n', '<leader>ft', '<cmd>TodoTelescope<cr>', { desc = 'Find todos' })
+      keymap.set('n', '<leader>en', function()
+         require('telescope.builtin').find_files {
+            cwd = vim.fn.stdpath 'config',
+         }
+      end)
    end,
 }
