ee3eec8bd8b07ac587021a2bcd3dc42ec1a906d6
Jordan Palmer <jordan.m.palmer@boeing.com>
2025-03-22 11:40:14 -0400

    splitting out lazy install

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 require 'core'
--- require 'lazy'
+require 'lazy-vim'
 
 vim.o.clipboard = 'unnamedplus'
 vim.g.clipboard = {
@@ -16,32 +16,6 @@ vim.g.clipboard = {
    },
    cache_enabled = 0,
 }
-
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-   local out = vim.fn.system {
-      'git',
-      'clone',
-      '--filter=blob:none',
-      '--branch=stable',
-      'https://github.com/folke/lazy.nvim.git',
-      lazypath,
-   }
-   if vim.v.shell_error ~= 0 then
-      error('Error cloning lazy.nvim:\n' .. out)
-   end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup({ { import = 'plugins' }, { import = 'plugins.lsp' } }, {
-   checker = {
-      enabled = true,
-      notify = false,
-   },
-   change_detection = {
-      notify = false,
-   },
-})
 
 vim.opt.termguicolors = true
 if vim.fn.has 'termguicolors' == 1 then
