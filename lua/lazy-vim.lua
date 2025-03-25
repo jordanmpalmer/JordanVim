ca63ffa02bbf7e9872971f12ddb468eed8b3bc6f
Jordan Palmer <jordan.m.palmer@boeing.com>
2025-03-25 10:46:58 -0400

    fixed Mason not being available

--- lua/lazy-vim.lua
+++ lua/lazy-vim.lua
@@ -14,7 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins', {
+require('lazy').setup({ { import = 'plugins' }, { import = 'plugins.lsp' } }, {
    checker = {
       enabled = true,
       notify = false,
